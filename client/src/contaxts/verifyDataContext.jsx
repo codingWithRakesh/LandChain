0dcb85afc1b01accb7552cd7e1570c2427f67b11
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2026-04-09 16:04:09 +0530

    connect 4 pages

--- client/src/contaxts/verifyDataContext.jsx
+++ client/src/contaxts/verifyDataContext.jsx
@@ -3,7 +3,11 @@ import React, { createContext, useContext, useState } from 'react'
 export const VerifyDataContext = createContext()
 
 const VerifyDataContextProvider = ({ children }) => {
-    const [verifyData, setVerifyData] = useState({});
+    const [verifyData, setVerifyData] = useState({
+        aadhaar: "399194356894",
+        name: "Tarapada Garai",
+        address: "Delhi, India",
+    });
     return (
         <VerifyDataContext.Provider value={{ verifyData, setVerifyData }}>
             {children}
